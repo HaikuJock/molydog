711787b2530440cb6b8388d29118bcd27fa01fee
jock <jock@kotikan.com>
2013-07-06 03:18:56 +0100

    WIP

---
+++ Assets/PointOfEmbarrassmentScript.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+
+public class PointOfEmbarrassmentScript : MonoBehaviour {
+	public float AttractionLevel = 0.0f;
+	public float EmbarrassmentLevel = 0.0f;
+	public float ObservedEmbarrassmentFactor = 0.0f;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
